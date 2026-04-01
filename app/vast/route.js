6be78506a03ead400dfc36eeadbd9ee705caf489
fb12-thonk <fb1202598@gmail.com>
2026-04-02 01:38:39 +0700

    Update route.js

--- app/vast/route.js
+++ app/vast/route.js
@@ -32,23 +32,28 @@ export async function GET(request) {
 
     const ad = data[0];
 
+    // ROMBAKAN XML: ID dipalsukan jadi angka 1, ditambah resolusi, dan tracker diubah ke format gambar transparan.
     const xmlTemplate = `<?xml version="1.0" encoding="UTF-8"?>
 <VAST version="3.0">
-  <Ad id="${ad.id}">
+  <Ad id="1">
     <InLine>
-      <AdSystem>Vidly Ad Network</AdSystem>
+      <AdSystem>Vidly88</AdSystem>
       <AdTitle><![CDATA[${ad.title}]]></AdTitle>
-      <Description><![CDATA[${ad.description || ''}]]></Description>
-      <Impression id="vidly-track"><![CDATA[https://httpbin.org/status/200]]></Impression>
+      <Description><![CDATA[${ad.description || 'Vidly Ad'}]]></Description>
+      
+      <Impression id="track">
+        <![CDATA[https://upload.wikimedia.org/wikipedia/commons/c/ce/Transparent.gif]]>
+      </Impression>
+      
       <Creatives>
-        <Creative sequence="1" id="kreatif-${ad.id}">
-          <Linear skipoffset="00:00:05">
+        <Creative sequence="1" id="1">
+          <Linear skipoffset="00:00:15">
             <Duration>00:00:30</Duration>
             <VideoClicks>
               <ClickThrough><![CDATA[${ad.click_link}]]></ClickThrough>
             </VideoClicks>
             <MediaFiles>
-              <MediaFile delivery="progressive" type="video/mp4">
+              <MediaFile delivery="progressive" type="video/mp4" width="1280" height="720">
                 <![CDATA[${ad.video_url}]]>
               </MediaFile>
             </MediaFiles>
@@ -59,12 +64,14 @@ export async function GET(request) {
   </Ad>
 </VAST>`;
 
+    // Menggunakan tipe data application/xml yang lebih diakui oleh player ketat
     return new NextResponse(xmlTemplate, {
       status: 200,
       headers: {
-        'Content-Type': 'text/xml; charset=utf-8',
+        'Content-Type': 'application/xml; charset=utf-8',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET, OPTIONS',
+        'Cache-Control': 'no-store, max-age=0' // Mencegah Vercel menyimpan cache versi lama
       },
     });
 
